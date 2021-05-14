93fd0ff8ad51e1bbf08bb6c166ff420e9ba3e36f
ikkix16 <57122624+ikkix16@users.noreply.github.com>
2021-05-14 02:40:07 -0700

    Merge perron
    Ya combinado

--- src/app/contactos/contactos.page.ts
+++ src/app/contactos/contactos.page.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { Observable } from 'rxjs';
+import { PerfilUsuarioComponent } from '../components/perfil-usuario/perfil-usuario.component';
 import { DataService } from '../services/data.service';
 
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.page.html',
-  styleUrls: ['./chat.page.scss'],
+  selector: 'app-contactos',
+  templateUrl: './contactos.page.html',
+  styleUrls: ['./contactos.page.scss'],
 })
 export class ContactosPage implements OnInit {
-  users: any[]=[];
+  usuarios: any[]=[];
   textoBuscar = '';
   
 
-  constructor(private dataService: DataService) { }
+  constructor(private modalCtrl: ModalController,private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.getUsers().subscribe(users =>{
+    this.dataService.getUsers().subscribe(usuarios =>{
 
-      console.log(users)
+      console.log(usuarios)
 
-      this.users=users;
+      this.usuarios=usuarios;
 
     });
 
@@ -28,6 +30,16 @@ export class ContactosPage implements OnInit {
    
 
   }
+
+  async verUsuarioPerfil(usuario){
+    const modal = await this.modalCtrl.create({
+      component: PerfilUsuarioComponent,
+      componentProps:{
+        usuario
+      }
+    })
+    modal.present();
+   }
   buscar(event){
     this.textoBuscar=event.detail.value
   }
